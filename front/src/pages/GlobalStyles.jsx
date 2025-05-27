1776a62f6db188efdf6d29b20db087b2c53c5a00
SrJacoby <luizjacoby294@gmail.com>
2025-05-27 17:19:25 -0300

    iniciando a implementação do modo escuro

--- front/src/pages/GlobalStyles.jsx
+++ front/src/pages/GlobalStyles.jsx
@@ -1,13 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-    *{
-        font-family: sans-serif;
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box
-    }
-
     body{
         background-color: ${({theme}) => theme.background};
         color: ${({theme}) => theme.text};
